76ce77c3476074ae7298436e121eb7ecc8d96f5a
vincentw1997 <vincentwahyudi1997@gmail.com>
2023-01-24 17:55:02 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Changes to be committed:
    	modified:   .gitignore
    	modified:   target/debug/deps/Projectinventory-38e0043ad885b2d2.d
    	modified:   target/debug/deps/Projectinventory-94572f9d46c82c35.d
    	deleted:    target/debug/incremental/Projectinventory-2xoij45jk6adt/s-ghkdan03jj-139hxnk-2u7x47762in6v/dep-graph.bin
    	deleted:    target/debug/incremental/Projectinventory-2xoij45jk6adt/s-ghkdan03jj-139hxnk-2u7x47762in6v/query-cache.bin
    	deleted:    target/debug/incremental/Projectinventory-2xoij45jk6adt/s-ghkdan03jj-139hxnk-2u7x47762in6v/work-products.bin
    	deleted:    target/debug/incremental/Projectinventory-2xoij45jk6adt/s-ghkdan03jj-139hxnk.lock
    	deleted:    target/debug/incremental/Projectinventory-30hgh7z99lvim/s-ghkdan03jj-1j3uuk0-28pxzd9djw6tl/dep-graph.bin
    	deleted:    target/debug/incremental/Projectinventory-30hgh7z99lvim/s-ghkdan03jj-1j3uuk0-28pxzd9djw6tl/query-cache.bin
    	deleted:    target/debug/incremental/Projectinventory-30hgh7z99lvim/s-ghkdan03jj-1j3uuk0-28pxzd9djw6tl/work-products.bin
    	deleted:    target/debug/incremental/Projectinventory-30hgh7z99lvim/s-ghkdan03jj-1j3uuk0.lock

--- target/debug/deps/Projectinventory-38e0043ad885b2d2.d
+++ target/debug/deps/Projectinventory-38e0043ad885b2d2.d
@@ -1,5 +1,5 @@
-/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/Projectinventory/target/debug/deps/Projectinventory-38e0043ad885b2d2.rmeta: src/main.rs
+/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/InventoryManagementRust/target/debug/deps/Projectinventory-38e0043ad885b2d2.rmeta: src/main.rs
 
-/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/Projectinventory/target/debug/deps/Projectinventory-38e0043ad885b2d2.d: src/main.rs
+/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/InventoryManagementRust/target/debug/deps/Projectinventory-38e0043ad885b2d2.d: src/main.rs
 
 src/main.rs:
--- target/debug/deps/Projectinventory-94572f9d46c82c35.d
+++ target/debug/deps/Projectinventory-94572f9d46c82c35.d
@@ -1,5 +1,5 @@
-/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/Projectinventory/target/debug/deps/Projectinventory-94572f9d46c82c35.rmeta: src/main.rs
+/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/InventoryManagementRust/target/debug/deps/Projectinventory-94572f9d46c82c35.rmeta: src/main.rs
 
-/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/Projectinventory/target/debug/deps/Projectinventory-94572f9d46c82c35.d: src/main.rs
+/Users/vincentwahyudi/Desktop/Vincent(new)/1. FH Würzburg Schweinfurt/Classes/2 Semester/1. Rust Programming for Safety Critical Systems/Project Rust/InventoryManagementRust/target/debug/deps/Projectinventory-94572f9d46c82c35.d: src/main.rs
 
 src/main.rs:
